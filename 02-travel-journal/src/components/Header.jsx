0558ec95f6eed27e5c84ca9c61b802d68f1e30c7
lorailkova <lorailkova23@gmail.com>
2026-03-19 19:08:25 +0200

    header styling

---
+++ 02-travel-journal/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <header>
+            <img src="images\globe.png" alt="globe image" />
+            <h1>my travel journal.</h1>
+        </header>
+    )
+}
