a46b9553ad1e0e141f80f3db0ff3c39a3dd7baa1
lorailkova <lorailkova23@gmail.com>
2026-03-19 17:09:30 +0200

    styling navigation section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 export default function Navbar(){
     return(
-        <nav>
-            <img className="nav-image" src="images\react-logo.png" alt="React Logo" />
-            <span className="nav-text">React Facts</span>
-        </nav>
+        <header>
+            <nav>
+                <img src="images\react-logo.png" alt="React Logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
     )
 }
