f5c84402e57c84369f71f9361cc55c95653f6139
lorailkova <lorailkova23@gmail.com>
2026-03-19 18:01:13 +0200

    styling main section

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,6 @@ import App from "./App"
 
 const root = createRoot(document.getElementById("root"))
 
-function Page(){
-    return(
-        <App />
-    )
-}
 root.render(
-    <Page />
+    <App />
 )
